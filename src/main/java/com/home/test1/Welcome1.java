4506f731169fc1025da6a754bc6945a212e943f4
GouravMajumder <gouravmajumder86953@gmail.com>
2022-12-28 22:08:24 +0530

    GM - SP02 Second Push

---
+++ src/main/java/com/home/test1/Welcome1.java
@@ -0,0 +1,10 @@
+package com.home.test1;
+
+public class Welcome1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Second Push");
+	}
+
+}
